f21c4628c5c8007f40e371accba5321a8a7464aa
Brian Eaton <eaton@alum.mit.edu>
2025-08-25 12:34:29 -0400

    switch to Read the Docs theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -51,8 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'camdoc'
-copyright = u'2017 UCAR'
-author = u'Brian Eaton, Steve Goldhaber'
+copyright = u'2025 UCAR'
+author = u'CAM developers and maintainers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -88,7 +88,8 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'bizstyle'
+#html_theme = 'bizstyle'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
